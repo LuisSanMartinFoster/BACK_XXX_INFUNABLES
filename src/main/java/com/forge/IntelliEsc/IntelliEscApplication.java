a597bc0d668cb687f171d63fbda625823ae2ad31
XIN9 <xin9@MacBook-Air-de-xin9.local>
2019-12-10 00:42:26 -0300

    primer commit

---
+++ src/main/java/com/forge/IntelliEsc/IntelliEscApplication.java
@@ -0,0 +1,13 @@
+package com.forge.IntelliEsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliEscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliEscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forge/IntelliEsc/IntelliEscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forge.IntelliEsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliEscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
